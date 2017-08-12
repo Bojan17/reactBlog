d05564f3b856592a18e9d3f1babfe89d3ed728a7
Bojan17 <bojan.kovacevic.pv@gmail.com>
2017-08-11 18:52:58 -0800

    inital commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import postReducer from './reducer_post';
+import {reducer as formReducer} from 'redux-form';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: postReducer,
+  form: formReducer
 });
 
 export default rootReducer;
